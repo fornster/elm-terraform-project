0c0a2c8bc1c29fd3037921bfb2a92af195d520b5
jakeforcade <jacob.forcade@integral.io>
2022-08-23 16:13:32 -0400

    init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx
+COPY public /etc/nginx
---
+++ buildAndPush.sh
@@ -0,0 +1,2 @@
+podman build . -t us-central1-docker.pkg.dev/spartan-vertex-360314/testing/elm-nginx-image:0.0.1
+podman push us-central1-docker.pkg.dev/spartan-vertex-360314/testing/elm-nginx-image:0.0.1
